69f054ec8f517e6b2923da94d29acef5cd14a0cb
aceisScope <bertieliu52@gmail.com>
2015-10-28 09:13:23 +0200

    turn off log

--- models/index.js
+++ models/index.js
@@ -11,7 +11,7 @@ if (!global.hasOwnProperty('db')) {
       protocol: 'postgres',
       port:     match[4],
       host:     match[3],
-      logging:  false
+      logging:  false,
       dialectOptions: {
           ssl: true
       }
