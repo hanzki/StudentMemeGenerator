467e61ba264ba617a6e7d0429ea3128ea8c15dd9
aceisScope <bertieliu52@gmail.com>
2015-11-14 14:23:55 +0200

    change association between memes and images to belongto

--- models/index.js
+++ models/index.js
@@ -13,16 +13,8 @@ if (!global.hasOwnProperty('db')) {
     posts:      sequelize.import(__dirname + '/post')
   };
 
-  /*
-    Associations can be defined here. E.g. like this:
-
-    global.db.memes.hasOne(global.db.images)
-    global.db.images.belongsTo(global.db.memes,{as:'image'})
-    global.db.memes.hasMany(global.db.meme_texts, {foreignKey: 'top_text_id'})
-    global.db.memes.hasMany(global.db.meme_texts, {foreignKey: 'bottom_text_id'})
-    global.db.meme_texts.belongsTo(global.db.memes)
-    */
-    global.db.images.hasMany(global.db.memes);
+  /* Associations */
+    //global.db.images.hasMany(global.db.memes);
     global.db.memes.belongsTo(global.db.images,{as:"Images"});
 
 
