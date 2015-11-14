e7927661fc25b0d057c0493b118aa33b4b12129d
Hannu Huhtanen <hannu.huhtanen@gmail.com>
2015-11-14 13:41:44 +0200

    Included angular ui-router

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -2,11 +2,30 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
+  'ui.router',
   'myApp.view1',
   'myApp.view2',
   'myApp.version'
 ]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+config( function($stateProvider, $urlRouterProvider, $locationProvider) {
+    $urlRouterProvider.otherwise('/')
+
+    $stateProvider.
+    state('view1', {
+        url: "/view1",
+        templateUrl: 'view1/view1.html',
+        controller: 'View1Ctrl',
+        name: "view1",
+    }).
+    state('view2', {
+        url: "/view2",
+        templateUrl: 'view2/view2.html',
+        controller: 'View2Ctrl',
+        name: "view2"
+    });
+
+  $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: false
+  });
+});
