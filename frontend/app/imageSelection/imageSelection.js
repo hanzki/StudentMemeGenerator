1a4edf54b6c1083d6235a24cfcf88a8057d98c69
Hannu Huhtanen <hannu.huhtanen@gmail.com>
2015-11-29 19:59:46 +0200

    Removed unused code

--- frontend/app/imageSelection/imageSelection.js
+++ frontend/app/imageSelection/imageSelection.js
@@ -5,13 +5,6 @@ angular.module('myApp.imageSelection', ["ui.router", "ngFileUpload"])
     .controller('ImageSelectionCtrl', ["$scope", "$state", "MemeService", "ImageService",
         function($scope, $state, MemeService, ImageService) {
 
-            function mockImage(url) {
-                return {
-                    id: Math.floor(Math.random() * 100),
-                    url: url
-                };
-            }
-
             $scope.chooseImage = function (id) {
                 console.log("chose image id=" + id);
                 var meme = MemeService.clearAndGetCurrentMeme();
@@ -35,17 +28,5 @@ angular.module('myApp.imageSelection', ["ui.router", "ngFileUpload"])
                     $scope.images = images;
                 }
             );
-
-                /*[
-                mockImage("https://i.imgflip.com/2/1bij.jpg"),
-                mockImage("https://i.imgflip.com/2/9ehk.jpg"),
-                mockImage("https://i.imgflip.com/2/1bh8.jpg"),
-                mockImage("https://i.imgflip.com/2/26am.jpg"),
-                mockImage("https://i.imgflip.com/2/1bgw.jpg"),
-                mockImage("https://i.imgflip.com/2/7g1q.jpg"),
-                mockImage("https://i.imgflip.com/2/39t1o.jpg"),
-                mockImage("https://i.imgflip.com/2/1bhf.jpg"),
-                mockImage("https://i.imgflip.com/2/1bh3.jpg")
-            ];*/
 
     }]);
